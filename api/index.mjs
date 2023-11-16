8c8861e38d58e38cf03b0ce695351b56d5c18cae
RightNxw <69996981+RightNxw@users.noreply.github.com>
2023-11-15 18:13:15 -0800

    Update server

--- api/index.mjs
+++ api/index.mjs
@@ -73,4 +73,9 @@ app.get("/api", async (req, res, next) => {
     next(error);
   }
 });
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
 export default app;
